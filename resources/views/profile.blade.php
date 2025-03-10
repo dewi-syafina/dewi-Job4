1aae6acac24b852ae22f077e9d99ff14978b5925
dewi-syafina <dewisyafinaaa118@gmail.com>
2025-03-10 12:42:45 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="al.png" alt="Profile Picture" width="150">
+                    <h3>Dewi Syafina</h3>
+                    <p>Email: dewisyafinaaa118@gmail.com</p>
+                    <p>Bio: Murid Marapthon</p>
+                    <h5>Skills:</h5>
+                    <ul>
+                        <li>Memasak</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
